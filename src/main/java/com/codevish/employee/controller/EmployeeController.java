1262ef8ea504147126cebfca0d2d83143e7e3b3f
Vishnu Chaturvedi <vishnu.chaturvedi.1002@gmail.com>
2022-05-04 21:56:16 +0530

    Initial commit

---
+++ src/main/java/com/codevish/employee/controller/EmployeeController.java
@@ -0,0 +1,16 @@
+package com.codevish.employee.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @GetMapping("/")
+    public String greet() {
+        return "Welcome to Employee API";
+    }
+
+}
